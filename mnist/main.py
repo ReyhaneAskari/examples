74d01b121eb64453f9c57ecb2c29a8804fb439bd
Reyhane Askari <r.askari.hemmat@gmail.com>
2017-12-14 00:03:18 -0500

    data augmentation happened

--- mnist/main.py
+++ mnist/main.py
@@ -23,6 +23,8 @@ parser.add_argument('--no-cuda', action='store_true', default=False,
                     help='disables CUDA training')
 parser.add_argument('--seed', type=int, default=1, metavar='S',
                     help='random seed (default: 1)')
+parser.add_argument('--bench_equalize', action='store_true', default=False,
+                    help='if true use both gpu and cpu')
 parser.add_argument(
     '--log-interval', type=int, default=10, metavar='N',
     help='howmany batches to wait before logging training status')
@@ -33,11 +35,14 @@ torch.manual_seed(args.seed)
 if args.cuda:
     torch.cuda.manual_seed(args.seed)
 
-
 kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
 train_loader = torch.utils.data.DataLoader(
     datasets.MNIST('../data', train=True, download=True,
                    transform=transforms.Compose([
+                       # a few useless rotations to equalize gpu and cpu usages
+                       transforms.RandomRotation([30, 70]),
+                       transforms.RandomRotation([30, 70]),
+                       transforms.RandomRotation([30, 70]),
                        transforms.ToTensor(),
                        transforms.Normalize((0.1307,), (0.3081,))
                    ])),
@@ -53,19 +58,29 @@ test_loader = torch.utils.data.DataLoader(
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv1 = nn.Conv2d(1, 100, kernel_size=5)
+        self.conv2 = nn.Conv2d(100, 50, kernel_size=5)
         self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
+        self.conv3 = nn.Conv2d(50, 50, kernel_size=5)
+        self.conv4 = nn.Conv2d(50, 50, kernel_size=5)
+        self.conv5 = nn.Conv2d(50, 50, kernel_size=5)
+        self.conv6 = nn.Conv2d(50, 50, kernel_size=5)
+        self.fc1 = nn.Linear(800, 500)
+        self.fc2 = nn.Linear(500, 50)
+        self.fc3 = nn.Linear(50, 10)
 
     def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2_drop(self.conv2(x)))
+        x = F.relu(self.conv3(x))
+        x = F.relu(self.conv4(x))
+        x = F.relu(self.conv5(x))
+        x = F.relu(self.conv6(x))
+        x = x.view(-1, 800)
         x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
         x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
+        x = self.fc3(x)
         return F.log_softmax(x)
 
 model = Net()
@@ -74,10 +89,10 @@ if args.cuda:
 
 optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 
-
-def train(epoch):
+def train(epoch, bench_equalize=False):
     model.train()
-    for batch_idx, (data, target) in enumerate(train_loader):
+
+    def train_step(batch_idx, data, target):
         if args.cuda:
             data, target = data.cuda(), target.cuda()
         data, target = Variable(data), Variable(target)
@@ -90,6 +105,14 @@ def train(epoch):
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.data[0]))
+
+    if bench_equalize:
+        for batch_idx, (data, target) in enumerate(train_loader):
+            train_step(batch_idx, data, target)
+    else:
+        data, target = iter(train_loader).next()
+        for batch_idx in range(len(train_loader)):
+            train_step(batch_idx, data, target)
 
 
 def test():
@@ -110,7 +133,10 @@ def test():
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
 
-
+if args.bench_equalize:
+    print('Both CPU and GPU are being used!')
+else:
+    print('Only GPU is being used!')
 for epoch in range(1, args.epochs + 1):
-    train(epoch)
+    train(epoch, args.bench_equalize)
     test()
